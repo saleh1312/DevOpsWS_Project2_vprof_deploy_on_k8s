34f942bd47204ca2456a36be5fcde9c22d8fdc05
saleh1312 <profsoft1312@gmail.com>
2025-03-07 16:36:00 +0200

    x

--- microservices/db01_data/main.sh
+++ microservices/db01_data/main.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-yum install epel-release -y
-yum install git mariadb-server expect -y
+echo "+++++ INSTALLING GIT AND MARIA"
+apt update
+apt install git expect -y
 
 
-systemctl start mariadb
-systemctl enable mariadb
-
+echo "+++++ CONFIGURING DATABASE"
 mysql -u root < /app/set_db_pass.sql
 
 expect /app/mysql_secure_install.exp
 
 mysql -u root -padmin123 < /app/accounts.sql
 
+echo "+++++ CLONING REPO"
 git clone -b main https://github.com/hkhcoder/vprofile-project.git
 cd vprofile-project
 mysql -u root -padmin123 accounts < src/main/resources/db_backup.sql
